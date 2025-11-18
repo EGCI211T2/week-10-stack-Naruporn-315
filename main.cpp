f4cc3c6d839f4bf9b2d333a69a28336278c31d61
Naruporn-315 <naruporn.kau@student.mahidol.edu>
2025-11-18 04:26:23 +0000

    Updated

--- main.cpp
+++ main.cpp
@@ -1,28 +1,25 @@
 #include<iostream>
 using namespace std;
-
+//#include <cstring>
 #include "stack.h"
 
 int main(int argc, char **argv){
     Stack s;
-     s.push(5);
-      s.push(1);
-      s.push(7);
-      s.pop();
-      s.push(6);
-      s.pop();
-
- /*
-  Exercise 1*/
-    /*
+      /*
+      s.push(5);  //5
+      s.push(1);  //1 5
+      s.push(7);  //7 1 5
+      s.pop();    //1 5
+      s.push(6);  //1 5
+      s.pop();    //5
+      */
+ 
+  //Exercise 1
     int i;
     for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
+        if(argv[i][0]=='x')  s.pop(); //or if(strcmp(agrv[i],"x")==0)
         else s.push(atoi(argv[i]));
     }
-     */
-
-
 
    return 0;
 }
--- stack.h
+++ stack.h
@@ -2,6 +2,7 @@
 #ifndef stack_h
 #define stack_h
 #include "node.h"
+
 class Stack {
 	NodePtr top;
 	int size;
@@ -14,31 +15,41 @@ public:
 
 
 void Stack::push(int x){
-  NodePtr new_node=new NODE(x);
+  NodePtr new_node =new NODE(x);
   if(new_node){
-            // Left missing for exercises…
+        new_node->set_next(top); //connect to the previous top
+        top=new_node;            //change top
+        size++;                  //increase size
    }
- 
-         // Left missing for exercises…
-    
 }
 
 int Stack::pop(){
         NodePtr t=top;
+        if(t!=NULL){
         int value;
         value=t->get_value();
-    // Left missing part for exercises
-        delete t;
+        top->get_next();      // move top to the next one
+        --size;               //decrease the size
+        delete t; 
         return value;
-	//be careful of the empty stack!!!
+        }
+        cout<<"Empty stack"<<endl;
+        return 0;          //be careful of the empty stack!!!
     }
 
 Stack::Stack(){
-    //initialize stack
+    top=NULL;
+    size=0;
     
 }
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
+    cout<<"Clear the stack";
+    int n=size;
+    while(n>0){
+        pop();
+        n--;
+    }
     
 }
 
